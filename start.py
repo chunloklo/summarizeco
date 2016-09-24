25a7e2ff2590dbe051df5b31f7a99912dfef7a36
Milind Lingineni <milind.lingineni@gmail.com>
2016-09-24 19:39:02 -0400

    Improved parsing

--- start.py
+++ start.py
@@ -1,5 +1,18 @@
 import subprocess
+import csv
 
 def callNews(companyName):
     s = 'py -2 news.py ' + companyName;
     subprocess.call(s, shell=True)
+
+def parseArticles():
+    d = {}
+    d['Title'] = []
+    d['URL'] = []
+    d['Date'] = []
+    dictReader = csv.DictReader(open('articles.txt', 'r'), fieldnames = ['Title', 'URL',
+        'Date'], delimiter = '^')
+    for row in dictReader:
+        for key in row:
+            d[key].append(row[key])
+    return d
