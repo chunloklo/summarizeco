40a6af214eec685f4c2adec3cec5cc1a8e32f89f
chunloklo <clo42@gatech.edu>
2016-09-25 00:39:10 -0400

    Still need to rename

--- senddata.py
+++ senddata.py
@@ -37,5 +37,15 @@ def testform_post():
         summary = processed_text, buzzWords = processed_buzz,
             jobList = jobList, news = news)
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 if __name__ == '__main__':
     app.run()
